a21dac07f28f7966b692c5a1b6fdaa612707559d
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2021-10-25 10:24:41 -0400

    javascript functions updated
    Now the function will change the text when the nav buttons are clicked.

--- js/script.js
+++ js/script.js
@@ -5,16 +5,43 @@ var headers = [
     "If you would like to contact me, please see the information below. Let's work together!",
 ];
 
+var images = [
 
+];
 
-function changeDiv(section) {
-    document.getElementById("h1").innerHTML=headers[section];
+var captions = [
+    ["I love helping people learn the language of music!" , "I love to create. Whether it is music arrangements, html emails, or complete programs.", "I love to help others. My job is to help YOUR life easier.",],
+    [],
+    [],
+    [],
+];
+
+function changeHeader (element) {
+    document.getElementById('heading').innerHTML = headers[element];
+};
+function changeImages (element){
+    document.getElementById('image').innerHTML = images[element];
+};
+function changeCaptions (element) {
+    document.getElementById('caption-1').innerHTML = captions[element][0];
+    document.getElementById('caption-2').innerHTML = captions[element][1];
+    document.getElementById('caption-3').innerHTML = captions[element][2];
 };
 
-document.getElementById("0").addEventListener("click", function {document.getElementById("h1").innerHTML= headers[0]});
-document.getElementById("1").addEventListener("click", changeDiv(1);
-document.getElementById("2").addEventListener("click", function {document.getElementById("h1").innerHTML= headers[2]});
-document.getElementById("3").addEventListener("click", function {document.getElementById("h1").innerHTML= headers[3]});
 
-
-
+document.getElementById('0').addEventListener("click", function(){
+    changeHeader(0);
+    changeCaptions(0);
+});
+document.getElementById('1').addEventListener("click", function(){
+    changeHeader(1);
+    changeCaptions(1);
+});
+document.getElementById('2').addEventListener("click", function(){
+    changeHeader(2);
+    changeCaptions(2);
+});
+document.getElementById('3').addEventListener("click", function(){
+    changeHeader(3);
+    changeCaptions(3);
+});
