c4457d63fb5bc68df4c2c03bd56ca16c9ba752f4
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2021-10-28 15:28:15 -0400

    Cleaned up javascript code

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
-var headers = [
+// site contents
+const headers = [
     "Hi. I am Tyler Baker, and I am a developer, composer, and musician.",
     "I am currently teaching music and studying front-end development techniques.",
     "Below you will find some example projects that I have created.",
     "If you would like to contact me, please see the information below. Let's work together!",
 ];
 
-var images = [
-
+const images = [
+    "images/tyler-baker-photo-1.png"
 ];
 
-var captions = [
+const captions = [
     ["I love helping people learn the language of music!" , "I love to create. Whether it is music arrangements, html emails, or complete programs.", "I love to help others. My job is to help YOUR life easier.",],
     [],
     [],
     [],
 ];
 
-function changeHeader (element) {
+// functions to create dynamic changing of content
+function changeElements (element) {
     document.getElementById('heading').innerHTML = headers[element];
-};
-function changeImages (element){
-    document.getElementById('image').innerHTML = images[element];
-};
-function changeCaptions (element) {
+    img = document.getElementById('heading-image');
+    img.src = images[element];
     document.getElementById('caption-1').innerHTML = captions[element][0];
     document.getElementById('caption-2').innerHTML = captions[element][1];
     document.getElementById('caption-3').innerHTML = captions[element][2];
 };
 
+function updateSite (element) {
+    let x = element.id
+    document.getElementById(x).addEventListener("click", function () {
+        changeElements(x);
+    })
 
-document.getElementById('0').addEventListener("click", function(){
-    changeHeader(0);
-    changeCaptions(0);
-});
-document.getElementById('1').addEventListener("click", function(){
-    changeHeader(1);
-    changeCaptions(1);
-});
-document.getElementById('2').addEventListener("click", function(){
-    changeHeader(2);
-    changeCaptions(2);
-});
-document.getElementById('3').addEventListener("click", function(){
-    changeHeader(3);
-    changeCaptions(3);
-});
+}
+
+// makes the nav buttons work
+let navlist = document.querySelectorAll("li.navlist");
+navlist.forEach(updateSite);
